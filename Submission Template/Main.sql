72912008badf2a942d318398b8dc0eecbc624618
Blake <48078206+Blakevdw@users.noreply.github.com>
2019-08-13 10:37:21 +1000

    Task 3 Complete
    Data Inserted

--- Submission Template/Main.sql
+++ Submission Template/Main.sql
@@ -24,10 +24,22 @@ GO
 CREATE TABLE dbo.Tour
 (
     TourName [NVARCHAR](50) NOT NULL PRIMARY KEY,
-    Description [NVARCHAR](50) NOT NULL
+    Description [NVARCHAR](100) NOT NULL
 );
 GO
 
+INSERT INTO dbo.Tour
+    ([TourName], [Description])
+
+VALUES
+    ('North', 'Tour of wineries and outlets of the Bedigo and Castlemaine region'),
+    ('South', 'Tour of wineries and outlets of Mornington Penisula'),
+    ('West', 'Tour of wineries and outlets of the Geelong and Otways region')
+
+GO
+
+SELECT *
+FROM DBO.TOUR
 
 IF OBJECT_ID('dbo.client', 'U') IS NOT NULL
 DROP TABLE dbo.client
@@ -41,6 +53,21 @@ CREATE TABLE dbo.client
     Gender [NVARCHAR] (50) NOT NULL,
 );
 GO
+
+INSERT INTO dbo.Client
+    ([ClientID], [Surname], [GivenName], [Gender])
+
+VALUES
+    (1, 'Price', 'Taylor', 'M'),
+    (2, 'Gamble', 'Ellyse', 'F'),
+    (3, 'Tan', 'Tilly', 'F'),
+    (102535342, 'van der Wal', 'Blake', 'M')
+
+GO
+
+SELECT *
+FROM dbo.Client
+
 IF OBJECT_ID('dbo.Event', 'U') IS NOT NULL
 DROP TABLE dbo.Event
 GO
@@ -57,11 +84,27 @@ CREATE TABLE dbo.Event
 );
 GO
 
+INSERT INTO dbo.Event
+    ([EventYear], [EventMonth], [EventDay], [Fee], [TourName])
+
+VALUES
+    (2016, 'Jan', 9, 200, 'North'),
+    (2016, 'Feb', 13, 225, 'North'),
+    (2016, 'Jan', 9, 200, 'South'),
+    (2016, 'Jan', 16, 200, 'South'),
+    (2016, 'Jan', 29, 225, 'West')
+
+    GO
+
+SELECT *
+from dbo.Event
+
+
 IF OBJECT_ID('dbo.Booking', 'U') IS NOT NULL
 DROP TABLE dbo.Booking
 GO
 
-CREATE TABLE dbo.Booking 
+CREATE TABLE dbo.Booking
 (
     DateBooked INT,
     Payment INT,
@@ -77,3 +120,23 @@ CREATE TABLE dbo.Booking
 
 );
 GO
+
+INSERT INTO dbo.Booking
+    ([DateBooked], [Payment], [ClientID], [EventYear], [EventMonth], [EventDay], [TourName], [Fee])
+
+VALUES
+    (10/12/2015, 200, 1, 2016, 'Jan', 9, 'North', 200),
+    (16/12/2015, 200, 2, 2016, 'Jan', 9, 'North', 200),
+    (08/01/2016, 225, 1, 2016, 'Feb', 13, 'North', 200),
+    (14/01/2016, 125, 2, 2016, 'Feb', 13, 'North', 125),
+    (03/02/2016, 225, 3, 2016, 'Feb', 13, 'North', 225),
+    (10/12/2015, 200, 1, 2016, 'Jan', 9, 'South', 200),
+    (18/12/2015, 200, 2, 2016, 'Jan', 16, 'South', 200),
+    (09/01/2016, 200, 3, 2016, 'Jan', 16, 'South', 200),
+    (17/12/2015, 225, 2, 2016, 'Jan', 29, 'West', 225),
+    (18/12/2015, 200, 3, 2016 , 'Jan', 29, 'West', 200)
+
+    GO
+
+SELECT *
+from dbo.Booking
